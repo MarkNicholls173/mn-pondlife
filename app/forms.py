f0e641af1f4cd91c011db8f05d51a7577b8973d8
Mark Nicholls <mark.nicholls@dvsa.gov.uk>
2022-09-07 08:25:46 +0100

    refactor part 1 complete

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from ast import Sub
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('Remeber Me')
+    submit = SubmitField('Sign In')
